fef3923ede42f898ec8b53b73b5e8c2c85753c99
Alex Porcelli <alex@porcelli.me>
2026-03-22 17:42:21 -0400

    release: [try] additional adjusts

--- packages/dmn-marshaller-backend-compatibility-tester/src/DmnMarshallerBackendCompatibilityTesterScript.java
+++ packages/dmn-marshaller-backend-compatibility-tester/src/DmnMarshallerBackendCompatibilityTesterScript.java
@@ -19,7 +19,7 @@
 
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 17
-//REPOS droolsAndKogito=file://${droolsAndKogitoLocalM2Repo},mavencentral
+//REPOS apachestaging=https://repository.apache.org/content/repositories/staging/,droolsAndKogito=file://${droolsAndKogitoLocalM2Repo},mavencentral
 //DEPS org.kie:drools-build-parent:${kogito-runtime.version}@pom
 //DEPS ch.qos.logback:logback-classic:1.2.13
 //DEPS info.picocli:picocli:4.7.5
